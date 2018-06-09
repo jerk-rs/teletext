e244cfeab06bf3a826e538839e4907f1943961f2
Ross Nomann <rossnomann@protonmail.com>
2018-06-09 17:27:20 +0300

    Full rewrite

---
+++ src/app.rs
@@ -0,0 +1,137 @@
+use futures::prelude::*;
+use std::{error::Error, fmt, io::Error as IoError};
+use telegram_bot::prelude::*;
+use telegram_bot::{Api, Error as TelegramError, Message, MessageEntity, MessageEntityKind,
+                   MessageKind, ParseMode, UpdateKind};
+use tokio_core::reactor::Core;
+use transform::*;
+
+pub type AppResult<T> = Result<T, AppError>;
+
+pub struct App {
+    api: Api,
+    core: Core,
+}
+
+impl App {
+    pub fn new(token: &str) -> AppResult<App> {
+        let core = Core::new()?;
+        let api = Api::configure(token).build(core.handle())?;
+        Ok(App {
+            api: api,
+            core: core,
+        })
+    }
+
+    pub fn run(mut self) -> AppResult<()> {
+        self.core.run(handle_updates(self.api))?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct AppError {
+    description: String,
+}
+
+impl AppError {
+    fn unknown_command(cmd: &str) -> AppError {
+        AppError {
+            description: format!("Unknown command: {}", cmd),
+        }
+    }
+}
+
+impl From<IoError> for AppError {
+    fn from(err: IoError) -> AppError {
+        AppError {
+            description: err.to_string(),
+        }
+    }
+}
+
+impl From<TelegramError> for AppError {
+    fn from(err: TelegramError) -> AppError {
+        AppError {
+            description: err.to_string(),
+        }
+    }
+}
+
+impl From<TransformError> for AppError {
+    fn from(err: TransformError) -> AppError {
+        AppError {
+            description: err.to_string(),
+        }
+    }
+}
+
+impl Error for AppError {
+    fn description(&self) -> &str {
+        &self.description
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(out, "{}", self.description)
+    }
+}
+
+#[async]
+fn handle_updates(api: Api) -> Result<(), TelegramError> {
+    #[async]
+    for update in api.stream() {
+        if let UpdateKind::Message(message) = update.kind {
+            match handle_command(&message) {
+                Ok(None) => { /* noop */ }
+                Ok(Some(reply)) => {
+                    if let Err(err) = await!(
+                        api.send(
+                            message
+                                .text_reply(format!("```\n{}\n```", reply))
+                                .parse_mode(ParseMode::Markdown),
+                        )
+                    ) {
+                        println!("Failed to send message: {}", err);
+                    }
+                }
+                Err(err) => {
+                    if let Err(err) = await!(api.send(message.text_reply(err.to_string()))) {
+                        println!("Failed to send message: {}", err);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn handle_command(message: &Message) -> Result<Option<String>, AppError> {
+    if let MessageKind::Text {
+        ref data,
+        ref entities,
+    } = message.kind
+    {
+        if let Some(MessageEntity {
+            kind: MessageEntityKind::BotCommand,
+            offset: 0,
+            length,
+        }) = entities.first()
+        {
+            let (cmd, text) = data.split_at(*length as usize);
+            let cmd = cmd.split('@').next();
+            if let Some(cmd) = cmd {
+                let text = text.trim();
+                return Ok(Some(match cmd {
+                    "/arrow" => text.to_arrow()?,
+                    "/square" => text.to_square()?,
+                    "/star" => text.to_star()?,
+                    "/sw" => text.to_sw()?,
+                    _ => return Err(AppError::unknown_command(cmd)),
+                }));
+            }
+        }
+    }
+    Ok(None)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,13 @@
-#![feature(proc_macro, generators, try_from)]
-
 extern crate dotenv;
-extern crate futures_await as futures;
-extern crate telegram_bot;
 extern crate teletext;
-extern crate tokio_core;
 
 use dotenv::dotenv;
-use futures::prelude::*;
-use std::convert::TryInto;
 use std::env;
-use telegram_bot::prelude::*;
-use telegram_bot::{Api, Message, MessageEntity, MessageEntityKind, MessageKind, ParseMode,
-                   UpdateKind};
-use teletext::{Arrow, Square, Star, Sw};
-use tokio_core::reactor::Core;
+use teletext::App;
 
 fn main() {
     dotenv().ok();
-    let mut core = Core::new().expect("Failed to create core");
     let token = env::var("TELETEXT_TOKEN").expect("Can not to get token");
-    let api = Api::configure(token).build(core.handle()).expect("Failed to build API");
-    core.run(handle_updates(api)).expect("Run failed");
-}
-
-#[async]
-fn handle_updates(api: Api) -> Result<(), telegram_bot::Error> {
-    #[async]
-    for update in api.stream() {
-        if let UpdateKind::Message(message) = update.kind {
-            if let Ok(reply) = handle_command(&message) {
-                if let Err(err) = await!(
-                    api.send(
-                        message
-                            .text_reply(format!("```\n{}\n```", reply))
-                            .parse_mode(ParseMode::Markdown),
-                    )
-                ) {
-                    println!("Failed to send message: {}", err);
-                }
-            }
-        }
-    }
-    Ok(())
-}
-
-fn handle_command(message: &Message) -> Result<String, ()> {
-    if let MessageKind::Text {
-        ref data,
-        ref entities,
-    } = message.kind
-    {
-        if let Some(MessageEntity {
-            kind: MessageEntityKind::BotCommand,
-            offset: 0,
-            length,
-        }) = entities.first()
-        {
-            let (cmd, text) = data.split_at(*length as usize);
-            let cmd = cmd.split('@').next();
-            if let Some(cmd) = cmd {
-                let text = text.trim();
-                return Ok(match cmd {
-                    "/arrow" => text.try_into()
-                        .unwrap_or(Arrow {
-                            buf: "goforkurself".into(),
-                        })
-                        .to_string(),
-                    "/square" => text.try_into()
-                        .unwrap_or(Square {
-                            buf: "goforkurself".into(),
-                        })
-                        .to_string(),
-                    "/star" => text.try_into()
-                        .unwrap_or(Star {
-                            buf: "goforkurself".into(),
-                        })
-                        .to_string(),
-                    "/sw" => text.try_into()
-                        .unwrap_or(Sw {
-                            buf: "goforkurself".into(),
-                        })
-                        .to_string(),
-                    _ => return Err(()),
-                });
-            }
-        }
-    }
-    Err(())
+    let app = App::new(&token).expect("Failed to create app");
+    app.run().expect("Run failed");
 }
