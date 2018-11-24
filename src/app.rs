e7973d43eb81c99a779ca1ea0fae44d5f86ddad5
mexus <gilaldpellaeon@gmail.com>
2018-11-24 15:35:36 +0100

    Get rid of handle_figure macro, get rid of To* traits

--- src/app.rs
+++ src/app.rs
@@ -1,60 +1,59 @@
 use huify::huify_sentence;
 use std::fmt;
 use teleborg::objects::{Chat, Message, Update};
-use teleborg::{Bot, Dispatcher, ParseMode, Updater};
-use transform::*;
+use teleborg::{Bot, Command, Dispatcher, ParseMode, Updater};
+use transform;
 
 pub fn run<S: Into<String>>(token: S) {
     let mut dispatcher = Dispatcher::new();
-    dispatcher.add_command_handler("arrow", handle_arrow, true);
+    dispatcher.add_command_handler("arrow", TransformCommand(transform::to_arrow), true);
     dispatcher.add_command_handler("huify", handle_huify, true);
-    dispatcher.add_command_handler("square", handle_square, true);
-    dispatcher.add_command_handler("star", handle_star, true);
-    dispatcher.add_command_handler("sw", handle_sw, true);
+    dispatcher.add_command_handler("square", TransformCommand(transform::to_square), true);
+    dispatcher.add_command_handler("star", TransformCommand(transform::to_star), true);
+    dispatcher.add_command_handler("sw", TransformCommand(transform::to_sw), true);
     Updater::start(Some(token.into()), None, None, None, dispatcher);
 }
 
-macro_rules! handle_figure {
-    ($name:ident, $transform:ident) => {
-        fn $name(bot: &Bot, update: Update, args: Option<Vec<&str>>) {
-            if let Some(Message {
-                message_id,
-                chat: Chat { id: chat_id, .. },
-                reply_to_message,
-                ..
-            }) = update.message
-            {
-                let result = match args {
-                    Some(args) => match args.join(" ").trim().$transform() {
-                        Ok(result) => CommandResult::new(result, true),
-                        Err(err) => CommandResult::new(err.to_string(), false),
-                    },
-                    None => CommandResult::new("You should provide some text", false),
-                };
-                let reply_to_id = match reply_to_message {
-                    Some(r) => r.message_id,
-                    None => message_id,
-                };
-                if let Err(err) = bot.send_message(
-                    &chat_id,
-                    &result.to_string(),
-                    Some(&ParseMode::Markdown),
-                    None,
-                    None,
-                    Some(&reply_to_id),
-                    None,
-                ) {
-                    println!("Failed to send a message: {:?}", err);
-                }
+pub struct TransformCommand<T>(T);
+
+impl<T, E> Command for TransformCommand<T>
+where
+    T: FnMut(&str) -> Result<String, E> + Send + Sync + 'static,
+    E: ToString,
+{
+    fn execute(&mut self, bot: &Bot, update: Update, args: Option<Vec<&str>>) {
+        if let Some(Message {
+            message_id,
+            chat: Chat { id: chat_id, .. },
+            reply_to_message,
+            ..
+        }) = update.message
+        {
+            let result = match args {
+                Some(args) => match (self.0)(args.join(" ").trim()) {
+                    Ok(result) => CommandResult::new(result, true),
+                    Err(err) => CommandResult::new(err.to_string(), false),
+                },
+                None => CommandResult::new("You should provide some text", false),
+            };
+            let reply_to_id = match reply_to_message {
+                Some(r) => r.message_id,
+                None => message_id,
+            };
+            if let Err(err) = bot.send_message(
+                &chat_id,
+                &result.to_string(),
+                Some(&ParseMode::Markdown),
+                None,
+                None,
+                Some(&reply_to_id),
+                None,
+            ) {
+                println!("Failed to send a message: {:?}", err);
             }
         }
-    };
+    }
 }
-
-handle_figure!(handle_arrow, to_arrow);
-handle_figure!(handle_square, to_square);
-handle_figure!(handle_star, to_star);
-handle_figure!(handle_sw, to_sw);
 
 fn handle_huify(bot: &Bot, update: Update, args: Option<Vec<&str>>) {
     let args: Option<String> = args
--- src/transform.rs
+++ src/transform.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, fmt};
 
 type Bounds = (usize, usize);
+
 const ARROW_BOUNDS: Bounds = (3, 100);
 const SQUARE_BOUNDS: Bounds = (2, 100);
 const STAR_BOUNDS: Bounds = (3, 100);
@@ -8,220 +9,196 @@ const SW_BOUNDS: Bounds = (3, 100);
 
 pub type TransformResult<T> = Result<T, TransformError>;
 
-pub trait ToArrow {
-    fn to_arrow(&self) -> TransformResult<String>;
+pub fn to_arrow(orig: &str) -> TransformResult<String> {
+    let len = orig.len();
+    if len < ARROW_BOUNDS.0 || len > ARROW_BOUNDS.1 {
+        return Err(TransformError::invalid_length(
+            ARROW_BOUNDS.0,
+            ARROW_BOUNDS.1,
+        ));
+    }
+
+    let chars = collect_chars(&orig);
+    let len = chars.len();
+    let mut buf = String::new(); // TODO: calc capacity
+
+    // top line
+    for (i, &c) in chars.iter().enumerate() {
+        buf.push(c);
+        if i == len - 1 {
+            buf.push('\n')
+        } else {
+            buf.push(' ')
+        }
+    }
+
+    // bottom lines
+    for (i, &c) in chars.iter().skip(1).enumerate() {
+        buf.push(c);
+        buf.push(' ');
+        for _ in 0..i * 2 {
+            buf.push(' ');
+        }
+        buf.push(c);
+        buf.push('\n');
+    }
+
+    Ok(buf)
 }
 
-impl<'a> ToArrow for &'a str {
-    fn to_arrow(&self) -> TransformResult<String> {
-        let len = self.len();
-        if len < ARROW_BOUNDS.0 || len > ARROW_BOUNDS.1 {
-            return Err(TransformError::invalid_length(
-                ARROW_BOUNDS.0,
-                ARROW_BOUNDS.1,
-            ));
-        }
+pub fn to_square(origin: &str) -> TransformResult<String> {
+    let len = origin.len();
+    if len < SQUARE_BOUNDS.0 || len > SQUARE_BOUNDS.1 {
+        return Err(TransformError::invalid_length(
+            SQUARE_BOUNDS.0,
+            SQUARE_BOUNDS.1,
+        ));
+    }
 
-        let chars = collect_chars(&self);
-        let len = chars.len();
-        let mut buf = String::new(); // TODO: calc capacity
-
-        // top line
-        for (i, &c) in chars.iter().enumerate() {
-            buf.push(c);
-            if i == len - 1 {
-                buf.push('\n')
-            } else {
-                buf.push(' ')
-            }
-        }
-
-        // bottom lines
-        for (i, &c) in chars.iter().skip(1).enumerate() {
-            buf.push(c);
-            buf.push(' ');
-            for _ in 0..i * 2 {
+    let chars = collect_chars(&origin);
+    let len = chars.len();
+    let side = len * 2 - 1;
+    let area = side * side;
+    let mut buf = String::with_capacity(area * 2 - 1);
+    let mut row_idx;
+    let mut col_idx;
+    for row in 0..side {
+        row_idx = if row >= len { side - row - 1 } else { row };
+        for col in 0..side {
+            col_idx = if col >= len { side - col - 1 } else { col };
+            buf.push(chars[len - 1 - if row_idx <= col_idx { row_idx } else { col_idx }]);
+            if col != side - 1 {
                 buf.push(' ');
             }
-            buf.push(c);
+        }
+        if row != side - 1 {
             buf.push('\n');
         }
-
-        Ok(buf)
     }
+    Ok(buf)
 }
 
-pub trait ToSquare {
-    fn to_square(&self) -> TransformResult<String>;
-}
-
-impl<'a> ToSquare for &'a str {
-    fn to_square(&self) -> TransformResult<String> {
-        let len = self.len();
-        if len < SQUARE_BOUNDS.0 || len > SQUARE_BOUNDS.1 {
-            return Err(TransformError::invalid_length(
-                SQUARE_BOUNDS.0,
-                SQUARE_BOUNDS.1,
-            ));
-        }
-
-        let chars = collect_chars(&self);
-        let len = chars.len();
-        let side = len * 2 - 1;
-        let area = side * side;
-        let mut buf = String::with_capacity(area * 2 - 1);
-        let mut row_idx;
-        let mut col_idx;
-        for row in 0..side {
-            row_idx = if row >= len { side - row - 1 } else { row };
-            for col in 0..side {
-                col_idx = if col >= len { side - col - 1 } else { col };
-                buf.push(chars[len - 1 - if row_idx <= col_idx { row_idx } else { col_idx }]);
-                if col != side - 1 {
-                    buf.push(' ');
-                }
-            }
-            if row != side - 1 {
-                buf.push('\n');
-            }
-        }
-        Ok(buf)
+pub fn to_star(orig: &str) -> TransformResult<String> {
+    let len = orig.len();
+    if len < STAR_BOUNDS.0 || len > STAR_BOUNDS.1 {
+        return Err(TransformError::invalid_length(STAR_BOUNDS.0, STAR_BOUNDS.1));
     }
-}
 
-pub trait ToStar {
-    fn to_star(&self) -> TransformResult<String>;
-}
+    let chars = collect_chars(&orig);
+    let len = chars.len();
+    let mut buf = String::new(); // TODO: calc capacity
 
-impl<'a> ToStar for &'a str {
-    fn to_star(&self) -> TransformResult<String> {
-        let len = self.len();
-        if len < STAR_BOUNDS.0 || len > STAR_BOUNDS.1 {
-            return Err(TransformError::invalid_length(STAR_BOUNDS.0, STAR_BOUNDS.1));
-        }
-
-        let chars = collect_chars(&self);
-        let len = chars.len();
-        let mut buf = String::new(); // TODO: calc capacity
-
-        // top lines
-        for (i, &c) in chars.iter().skip(1).enumerate().rev() {
-            for _ in 0..(len - i - 2) * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
-            buf.push(' ');
-            for _ in 0..i * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
-            buf.push(' ');
-            for _ in 0..i * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
-            buf.push('\n');
-        }
-
-        // middle line
-        for &c in chars.iter().skip(1).rev() {
-            buf.push(c);
+    // top lines
+    for (i, &c) in chars.iter().skip(1).enumerate().rev() {
+        for _ in 0..(len - i - 2) * 2 {
             buf.push(' ');
         }
-        for (i, &c) in chars.iter().enumerate() {
-            buf.push(c);
-            if i == len - 1 {
-                buf.push('\n')
-            } else {
-                buf.push(' ')
-            }
-        }
-
-        // bottom lines
-        for (i, &c) in chars.iter().skip(1).enumerate() {
-            for _ in 0..(len - i - 2) * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
+        buf.push(c);
+        buf.push(' ');
+        for _ in 0..i * 2 {
             buf.push(' ');
-            for _ in 0..i * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
-            buf.push(' ');
-            for _ in 0..i * 2 {
-                buf.push(' ');
-            }
-            buf.push(c);
-            buf.push('\n');
         }
-        Ok(buf)
+        buf.push(c);
+        buf.push(' ');
+        for _ in 0..i * 2 {
+            buf.push(' ');
+        }
+        buf.push(c);
+        buf.push('\n');
     }
-}
 
-pub trait ToSw {
-    fn to_sw(&self) -> TransformResult<String>;
-}
-
-impl<'a> ToSw for &'a str {
-    fn to_sw(&self) -> TransformResult<String> {
-        let len = self.len();
-        if len < SW_BOUNDS.0 || len > SW_BOUNDS.1 {
-            return Err(TransformError::invalid_length(SW_BOUNDS.0, SW_BOUNDS.1));
+    // middle line
+    for &c in chars.iter().skip(1).rev() {
+        buf.push(c);
+        buf.push(' ');
+    }
+    for (i, &c) in chars.iter().enumerate() {
+        buf.push(c);
+        if i == len - 1 {
+            buf.push('\n')
+        } else {
+            buf.push(' ')
         }
+    }
 
-        let chars = collect_chars(&self);
-        let len = chars.len();
-        let mut buf = String::new(); // TODO: calc capacity
+    // bottom lines
+    for (i, &c) in chars.iter().skip(1).enumerate() {
+        for _ in 0..(len - i - 2) * 2 {
+            buf.push(' ');
+        }
+        buf.push(c);
+        buf.push(' ');
+        for _ in 0..i * 2 {
+            buf.push(' ');
+        }
+        buf.push(c);
+        buf.push(' ');
+        for _ in 0..i * 2 {
+            buf.push(' ');
+        }
+        buf.push(c);
+        buf.push('\n');
+    }
+    Ok(buf)
+}
 
-        // top lines
-        for (a, b) in (0..len).zip((1..len).rev()) {
+pub fn to_sw(orig: &str) -> TransformResult<String> {
+    let len = orig.len();
+    if len < SW_BOUNDS.0 || len > SW_BOUNDS.1 {
+        return Err(TransformError::invalid_length(SW_BOUNDS.0, SW_BOUNDS.1));
+    }
+
+    let chars = collect_chars(&orig);
+    let len = chars.len();
+    let mut buf = String::new(); // TODO: calc capacity
+
+    // top lines
+    for (a, b) in (0..len).zip((1..len).rev()) {
+        buf.push(chars[a]);
+        buf.extend(vec![' '; (len - 2) * 2 + 1]);
+        if a == 0 {
+            for x in chars.iter().rev() {
+                buf.push(*x);
+                buf.push(' ');
+            }
+        } else {
+            buf.push(chars[b]);
+        }
+        buf.push('\n');
+    }
+
+    // middle line
+    for &c in chars.iter().skip(1).rev() {
+        buf.push(c);
+        buf.push(' ');
+    }
+    for (i, &c) in chars.iter().enumerate() {
+        buf.push(c);
+        if i == len - 1 {
+            buf.push('\n')
+        } else {
+            buf.push(' ')
+        }
+    }
+
+    // bottom lines
+    for (a, b) in (1..len).zip((0..len - 1).rev()) {
+        if b == 0 {
+            for x in chars.iter() {
+                buf.push(*x);
+                buf.push(' ');
+            }
+            buf.extend(vec![' '; (len - 2) * 2]);
+        } else {
+            buf.extend(vec![' '; (len - 1) * 2]);
             buf.push(chars[a]);
             buf.extend(vec![' '; (len - 2) * 2 + 1]);
-            if a == 0 {
-                for x in chars.iter().rev() {
-                    buf.push(*x);
-                    buf.push(' ');
-                }
-            } else {
-                buf.push(chars[b]);
-            }
-            buf.push('\n');
         }
-
-        // middle line
-        for &c in chars.iter().skip(1).rev() {
-            buf.push(c);
-            buf.push(' ');
-        }
-        for (i, &c) in chars.iter().enumerate() {
-            buf.push(c);
-            if i == len - 1 {
-                buf.push('\n')
-            } else {
-                buf.push(' ')
-            }
-        }
-
-        // bottom lines
-        for (a, b) in (1..len).zip((0..len - 1).rev()) {
-            if b == 0 {
-                for x in chars.iter() {
-                    buf.push(*x);
-                    buf.push(' ');
-                }
-                buf.extend(vec![' '; (len - 2) * 2]);
-            } else {
-                buf.extend(vec![' '; (len - 1) * 2]);
-                buf.push(chars[a]);
-                buf.extend(vec![' '; (len - 2) * 2 + 1]);
-            }
-            buf.push(chars[b]);
-            buf.push('\n');
-        }
-
-        Ok(buf)
+        buf.push(chars[b]);
+        buf.push('\n');
     }
+
+    Ok(buf)
 }
 
 #[derive(Debug)]
@@ -257,16 +234,20 @@ fn collect_chars(s: &str) -> Vec<char> {
 mod tests {
     use super::*;
 
-    fn assert_text_arrow(text: &str) {
-        let mut lines = text.lines();
+    #[test]
+    fn arrow() {
+        let transformed = to_arrow("text").unwrap();
+        let mut lines = transformed.lines();
         assert_eq!(lines.next(), Some("T E X T"));
         assert_eq!(lines.next(), Some("E E"));
         assert_eq!(lines.next(), Some("X   X"));
         assert_eq!(lines.next(), Some("T     T"));
     }
 
-    fn assert_text_square(text: &str) {
-        let mut lines = text.lines();
+    #[test]
+    fn square() {
+        let transformed = to_square("text").unwrap();
+        let mut lines = transformed.lines();
         assert_eq!(lines.next(), Some("T T T T T T T"));
         assert_eq!(lines.next(), Some("T X X X X X T"));
         assert_eq!(lines.next(), Some("T X E E E X T"));
@@ -276,8 +257,10 @@ mod tests {
         assert_eq!(lines.next(), Some("T T T T T T T"));
     }
 
-    fn assert_text_star(text: &str) {
-        let mut lines = text.lines();
+    #[test]
+    fn star() {
+        let transformed = to_star("text").unwrap();
+        let mut lines = transformed.lines();
         assert_eq!(lines.next(), Some("T     T     T"));
         assert_eq!(lines.next(), Some("  X   X   X"));
         assert_eq!(lines.next(), Some("    E E E"));
@@ -287,8 +270,10 @@ mod tests {
         assert_eq!(lines.next(), Some("T     T     T"));
     }
 
-    fn assert_text_sw(text: &str) {
-        let mut lines = text.lines();
+    #[test]
+    fn sw() {
+        let transformed = to_sw("rurust").unwrap();
+        let mut lines = transformed.lines();
         assert_eq!(lines.next(), Some("R         T S U R U R "));
         assert_eq!(lines.next(), Some("U         S"));
         assert_eq!(lines.next(), Some("R         U"));
@@ -300,25 +285,5 @@ mod tests {
         assert_eq!(lines.next(), Some("          U         R"));
         assert_eq!(lines.next(), Some("          S         U"));
         assert_eq!(lines.next(), Some("R U R U S T         R"));
-    }
-
-    #[test]
-    fn transform_arrow() {
-        assert_text_arrow(&"text".to_arrow().unwrap());
-    }
-
-    #[test]
-    fn transform_square() {
-        assert_text_square(&"text".to_square().unwrap());
-    }
-
-    #[test]
-    fn transform_star() {
-        assert_text_star(&"text".to_star().unwrap());
-    }
-
-    #[test]
-    fn transform_sw() {
-        assert_text_sw(&"rurust".to_sw().unwrap());
     }
 }
